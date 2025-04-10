b9ffcc479bb6cdb7fd81db5c72251feb1ca532fc
Yanxiang-Shan <y.shan@ufl.edu>
2025-04-10 14:32:25 -0400

    update

--- test_calculator.py
+++ test_calculator.py
@@ -22,7 +22,7 @@ class TestCalculator(unittest.TestCase):
             div(5, 0)
 
     def test_logarithm(self):
-        self.assertAlmostEqual(logarithm(10, 100), 2)
+        self.assertAlmostEqual(logarithm(10, 100), 0.5)
         self.assertAlmostEqual(logarithm(2, 8), 3)
 
     def test_log_invalid_base(self):
