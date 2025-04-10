d4b2eafdbcf29523407735015fbe18c214882ce9
Yanxiang-Shan <y.shan@ufl.edu>
2025-04-10 12:40:10 -0400

    modified calculator pl

--- calculator.py
+++ calculator.py
@@ -29,12 +29,6 @@ def logarithm(a,b):
 def exponent(a,b):
     return a**b
 
-def square_root(a):
-    if a<0:
-        raise ValueError
-    return a**(1/2)
 
-def hypotenuse(a,b):
-    return math.hypot(a,b)
 
 
