359c0910a504d4992943afe7fed35c93071118ea
Yanxiang-Shan <y.shan@ufl.edu>
2025-04-10 14:30:26 -0400

    update

--- test_calculator.py
+++ test_calculator.py
@@ -13,7 +13,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(0, 0), 0)
 
     def test_subtract(self):
-        self.assertEqual(subtract(10 ,4), 7)
+        self.assertEqual(subtract(10 ,4), 6)
         self.assertEqual(subtract(0, 5), -5)
         self.assertEqual(subtract(-3, -3), 0)
 
